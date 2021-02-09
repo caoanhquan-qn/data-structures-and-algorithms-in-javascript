04fe8270190055d0d6d2be8e87fcd8c43f8492a7
=caoanhquan <caoanhquan_qn@yahoo.com>
2021-02-09 22:02:57 +1100

    searching algorithms

--- script.js
+++ script.js
@@ -935,3 +935,395 @@ console.log(fib(28)); // 317811
 console.log(fib(35)); // 9227465
 
 console.log("///////////////");
+
+/* 
+
+Write a recursive function called reverse which accepts a string and returns
+a new string in reverse
+
+ */
+
+// function reverse(str) {
+//   return str.split("").reverse().join("");
+// }
+
+// function reverse(str) {
+//   let arr = [];
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     arr.push(str[i]);
+//   }
+//   return arr.join("");
+// }
+
+// function reverse(str) {
+//   let arr = [];
+//   function helper(helperInput) {
+//     if (helperInput.length === 0) {
+//       return;
+//     }
+//     arr.push(helperInput[helperInput.length - 1]);
+//     helper(helperInput.slice(0, -1));
+//   }
+//   helper(str);
+//   return arr.join("");
+// }
+
+function reverse(str) {
+  if (str.length <= 1) return str;
+  return reverse(str.slice(1)) + str[0];
+}
+// console.log(reverse("awesome"));
+// console.log(reverse("rithmschool"));
+
+/* 
+
+Write a recursive function called isPalindrome which returns true if the
+string passed to it is a palindrome (reads the same forward and backward). 
+Otherwise it returns false
+
+ */
+
+// multiple pointers pattern
+
+// function isPalindrome(str) {
+//   let i = 0;
+//   let j = str.length - 1;
+//   while (i < j) {
+//     if (str[i] !== str[j]) return false;
+//     if (str[i] === str[j]) {
+//       i++;
+//       j--;
+//     }
+//   }
+//   return true;
+// }
+
+// recursive
+
+// function isPalindrome(str) {
+//   if (str[0] !== str[str.length - 1]) return false;
+//   if (str.length <= 1) return true;
+//   return isPalindrome(str.slice(1, -1));
+// }
+
+function isPalindrome(str) {
+  if (str.length === 1) return true;
+  if (str.length === 2) return str[0] === str[1];
+  if (str[0] === str.slice(-1)) return isPalindrome(str.slice(1, -1));
+  return false;
+}
+
+// console.log(isPalindrome("awesome")); // false
+// console.log(isPalindrome("foobar")); // false
+// console.log(isPalindrome("tacocat")); // true
+// console.log(isPalindrome("amanaplanacanalpanama")); // true
+// console.log(isPalindrome("amanaplanacanalpandemonium")); // false
+
+/* 
+
+Write a recursive function called someRecursive which accepts an array and 
+a callback. The function returns true if a single value in the array returns
+true when passed to the callback. Otherwise it returns false 
+
+*/
+const isOdd = (val) => val % 2 !== 0;
+// function someRecursive(arr, callback) {
+//   for (let i = 0; i < arr.length; i++) {
+//     const result = callback(arr[i]);
+//     if (result) return true;
+//   }
+//   return false;
+// }
+
+// recursive
+function someRecursive(arr, callback) {
+  if (arr.length === 0) return false;
+  if (callback(arr[0])) return true;
+  return someRecursive(arr.slice(1), callback);
+}
+
+// console.log(someRecursive([1, 2, 3, 4], isOdd)); // true
+// console.log(someRecursive([4, 6, 8, 9], isOdd)); // true
+// console.log(someRecursive([4, 6, 8], isOdd)); // false
+// console.log(someRecursive([4, 6, 8], (val) => val > 10)); // false
+
+/* 
+
+Write a recursive function called flatten which accepts an array of arrays
+and returns a new array with all values flattened  
+
+*/
+
+// function flatten(arr) {
+//   return arr.flat(Infinity);
+// }
+
+// recursive
+// function flatten(arr) {
+//   return arr.reduce(
+//     (acc, val) => acc.concat(Array.isArray(val) ? flatten(val) : val),
+//     []
+//   );
+// }
+
+function flatten(oldArr) {
+  let newArr = [];
+  for (let i = 0; i < oldArr.length; i++) {
+    if (Array.isArray(oldArr[i])) {
+      newArr = newArr.concat(flatten(oldArr[i]));
+    } else {
+      newArr.push(oldArr[i]);
+    }
+  }
+  return newArr;
+}
+
+// console.log(flatten([1, 2, 3, [4, 5]])); // [1, 2, 3, 4, 5]
+// console.log(flatten([1, [2, [3, 4], [[5]]]])); // [1, 2, 3, 4, 5]
+// console.log(flatten([[1], [2], [3]])); // [1,2,3]
+// console.log(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]])); // [1,2,3]
+
+/* 
+
+Write a recursive function called capitalizeFirst. Given an array of strings,
+capitalize the first letter of each string in the array 
+
+*/
+// function capitalizeFirst(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     arr[i] = arr[i][0].toUpperCase() + arr[i].slice(1);
+//   }
+//   return arr;
+// }
+
+function capitalizeFirst(arr) {
+  let container = [];
+  (function helper(helperInput) {
+    if (helperInput.length === 0) return;
+    const val = helperInput[0][0].toUpperCase() + helperInput[0].slice(1);
+    container.push(val);
+    helper(helperInput.slice(1));
+  })(arr);
+  return container;
+}
+
+// console.log(capitalizeFirst(["car", "taco", "banana"]));
+
+/*
+
+Write a recursive function called nestedEvenSum. Return the sum of all even
+numbers in an object which may contain nested objects.
+
+*/
+
+/* let obj1 = {
+  outer: 2,
+  obj: {
+    inner: 2,
+    otherObj: {
+      superInner: 2,
+      notANumber: true,
+      alsoNotANumber: "yup",
+    },
+  },
+};
+
+let obj2 = {
+  a: 2,
+  b: { b: 2, bb: { b: 3, bb: { b: 2 } } },
+  c: { c: { c: 2 }, cc: "ball", ccc: 5 },
+  d: 1,
+  e: { e: { e: 2 }, ee: "car" },
+}; */
+
+function nestedEvenSum(obj) {
+  let sum = 0;
+  for (let key in obj) {
+    if (obj[key] instanceof Object) {
+      sum += nestedEvenSum(obj[key]);
+    } else if (Number.isFinite(obj[key]) && obj[key] % 2 === 0) {
+      sum += obj[key];
+    }
+  }
+  return sum;
+}
+// console.log(nestedEvenSum(obj1)); // 6
+// console.log(nestedEvenSum(obj2)); // 10
+
+/* 
+
+Write a recursive function called capitalizeWords. Given an array of words, return
+a new array containing each word capitalized
+
+*/
+// function capitalizeWords(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     arr[i] = arr[i].toUpperCase();
+//   }
+//   return arr;
+// }
+
+function capitalizeWords(arr) {
+  let container = [];
+
+  function helper(helperInput) {
+    if (helperInput.length === 0) return;
+    helperInput[0] = helperInput[0].toUpperCase();
+    container.push(helperInput[0]);
+    helper(helperInput.slice(1));
+  }
+  helper(arr);
+
+  return container;
+}
+// console.log(capitalizeWords(["i", "am", "learning", "recursion"]));
+// ["I", "AM", "LEARNING", "RECURSION"]
+
+/*
+
+ Write a function called stringifyNumbers which takes in an object and finds
+all of the values which are numbers and converts them to strings. Recursion
+would be a great way to solve this
+
+ */
+
+function stringifyNumbers(obj) {
+  let newObj = {};
+
+  for (let key in obj) {
+    if (obj[key] instanceof Object && !Array.isArray(obj[key])) {
+      newObj[key] = stringifyNumbers(obj[key]);
+    } else if (Number.isFinite(obj[key])) {
+      newObj[key] = obj[key].toString();
+    } else {
+      newObj[key] = obj[key];
+    }
+  }
+
+  return newObj;
+}
+
+// function stringifyNumbers(obj) {
+//   for (let key in obj) {
+//     if (obj[key] instanceof Object) {
+//       stringifyNumbers(obj[key]);
+//     } else if (Number.isFinite(obj[key])) {
+//       obj[key] = obj[key] + "";
+//     }
+//   }
+//   return obj;
+// }
+
+// let obj = {
+//   num: 1,
+//   test: [],
+//   data: {
+//     val: 4,
+//     info: {
+//       isRight: true,
+//       random: 66,
+//     },
+//   },
+// };
+
+// console.log(stringifyNumbers(obj));
+// console.log(obj);
+
+/*
+{
+    num: "1",
+    test: [],
+    data: {
+        val: "4",
+        info: {
+            isRight: true,
+            random: "66"
+        }
+    }
+}
+*/
+
+/* 
+Write a function called collectStrings which accepts an object and returns
+an array of all the values in the object that have a typeof string 
+*/
+
+function collectStrings(obj) {
+  let arr = [];
+
+  for (let key in obj) {
+    if (typeof obj[key] === "object") {
+      arr = arr.concat(collectStrings(obj[key]));
+    } else if (typeof obj[key] === "string") {
+      arr.push(obj[key]);
+    }
+  }
+
+  return arr;
+}
+
+const obj = {
+  stuff: "foo",
+  data: {
+    val: {
+      thing: {
+        info: "bar",
+        moreInfo: {
+          evenMoreInfo: {
+            weMadeIt: "baz",
+          },
+        },
+      },
+    },
+  },
+};
+
+// console.log(collectStrings(obj)); // ["foo", "bar", "baz"])
+console.log("///////////////");
+
+// Searching Algorithms
+
+/* 
+Write a function called linearSearch which accepts an array and a value,
+and returns the index at which the value exists. If the value does not exist
+in the array, return -1 
+*/
+
+function linearSearch(arr, num) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === num) {
+      return i;
+    }
+  }
+  return -1;
+}
+console.log(linearSearch([10, 15, 20, 25, 30], 15)); //1
+console.log(linearSearch([9, 8, 7, 6, 5, 4, 3, 2, 1, 0], 4)); //5
+console.log(linearSearch([100], 100)); // 0
+console.log(linearSearch([1, 2, 3, 4, 5], 6)); //-1
+console.log(linearSearch([9, 8, 7, 6, 5, 4, 3, 2, 1, 0], 10)); //-1
+console.log(linearSearch([100], 200)); //-1
+
+/* 
+
+Write a function called binarySearch which accepts a sorted array and a value
+and returns the index at which the value exists. Otherwise, return -1
+
+*/
+
+function binarySearch(arr, num) {
+  let i = 0;
+  let j = arr.length - 1;
+  while (i <= j) {
+    const middle = Math.floor((i + j) / 2);
+    const currentMiddle = arr[middle];
+    if (currentMiddle === num) return middle;
+    if (currentMiddle < num) {
+      i = middle + 1;
+    }
+    if (currentMiddle > num) {
+      j = middle - 1;
+    }
+  }
+  return -1;
+}
