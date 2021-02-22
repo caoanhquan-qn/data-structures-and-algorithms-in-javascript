964fb58286417f48b611412d9c4b71baf135484b
=caoanhquan <caoanhquan_qn@yahoo.com>
2021-02-22 21:19:07 +1100

    min binary heap and priority queue

--- script.js
+++ script.js
@@ -2302,3 +2302,147 @@ test case 2
 [39, 27, 33, 18, 12]
 
 */
+
+class MinBinaryHeap {
+  constructor() {
+    this.values = [];
+  }
+  insert(val) {
+    this.values.push(val);
+    this.bubbleUp();
+    return this.values;
+  }
+  bubbleUp() {
+    let idx = this.values.length - 1;
+    const element = this.values[idx];
+    while (idx > 0) {
+      let parentIdx = Math.floor((idx - 1) / 2);
+      let parent = this.values[parentIdx];
+      if (parent <= element) break;
+      this.values[parentIdx] = element;
+      this.values[idx] = parent;
+      idx = parentIdx;
+    }
+  }
+  extractMin() {
+    const min = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+    return min;
+  }
+  sinkDown() {
+    let idx = 0;
+    const length = this.values.length;
+    const element = this.values[0]; // keep track of this variable
+    while (true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChid, rightChild;
+      let swap = null;
+      if (leftChildIdx < length) {
+        leftChid = this.values[leftChildIdx];
+        if (element > leftChid) {
+          swap = leftChildIdx;
+        }
+      }
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx];
+        if (
+          (swap === null && rightChild < element) ||
+          (swap !== null && rightChild < leftChid)
+        ) {
+          swap = rightChildIdx;
+        }
+      }
+      if (swap === null) break;
+      this.values[idx] = this.values[swap];
+      this.values[swap] = element;
+      idx = swap;
+    }
+  }
+}
+let minheap = new MinBinaryHeap();
+
+// Priority Queue
+
+class PQNode {
+  constructor(val, priority) {
+    this.val = val;
+    this.priority = priority;
+  }
+}
+class PriorityQueue {
+  constructor() {
+    this.values = [];
+  }
+  enqueue(val, priority) {
+    let newNode = new PQNode(val, priority);
+    this.values.push(newNode);
+    this.bubbleUp();
+    return this.values;
+  }
+  bubbleUp() {
+    let idx = this.values.length - 1;
+    const element = this.values[idx];
+    while (idx > 0) {
+      let parentIdx = Math.floor((idx - 1) / 2);
+      let parent = this.values[parentIdx];
+      if (parent.priority <= element.priority) break;
+      this.values[parentIdx] = element;
+      this.values[idx] = parent;
+      idx = parentIdx;
+    }
+  }
+  dequeue() {
+    const min = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+    return min;
+  }
+  sinkDown() {
+    let idx = 0;
+    const length = this.values.length;
+    const element = this.values[0]; // keep track of this variable
+    while (true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChid, rightChild;
+      let swap = null;
+      if (leftChildIdx < length) {
+        leftChid = this.values[leftChildIdx];
+        if (element.priority > leftChid.priority) {
+          swap = leftChildIdx;
+        }
+      }
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx];
+        if (
+          (swap === null && rightChild.priority < element.priority) ||
+          (swap !== null && rightChild.priority < leftChid.priority)
+        ) {
+          swap = rightChildIdx;
+        }
+      }
+      if (swap === null) break;
+      this.values[idx] = this.values[swap];
+      this.values[swap] = element;
+      idx = swap;
+    }
+  }
+
+  print() {
+    return this.values.map((element) => `${element.val}: ${element.priority}`);
+  }
+}
+let priorityQueue = new PriorityQueue();
+priorityQueue.enqueue("nick", 5);
+priorityQueue.enqueue("steven", 1);
+priorityQueue.enqueue("zhang", 4);
+priorityQueue.enqueue("lee", 2);
+priorityQueue.enqueue("kento", 3);
